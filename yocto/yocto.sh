58954d25baff2732dd5c3054abc79e9bd39ced0b
a4a881d4 <a4a881d4@126.com>
2015-06-19 01:28:19 +0800

    add yocto.sh

---
+++ yocto/yocto.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/Xilinx/meta-xilinx.git
+git://git.openembedded.org/openembedded-core
+git clone git://git.yoctoproject.org/poky.git
+cd meta-xilinx
+git checkout 2af8d2a0e63aa371045895da03ba2bf98b51adb4
+patch < ../meta-xilinx/2af8d2a0e63aa371045895da03ba2bf98b51adb4/meta-xilinx.patch
+cd ../poky
+
